26a02192ab97d15dd9b9ac1627abbe52e448abe2
armando <abpprkonsalting@gmail.com>
2022-03-30 11:54:23 -0400

    seagoingexperience from operationalinformation

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\OperationalInformation;
+use App\Observers\OperationalInformationObsever;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -29,6 +31,6 @@ class EventServiceProvider extends ServiceProvider
     {
         parent::boot();
 
-        //
+        OperationalInformation::observe(OperationalInformationObsever::class);
     }
 }
