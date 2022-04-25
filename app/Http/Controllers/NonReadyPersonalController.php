1df6a3a162e5b059cd7cdf2c18717b4f2b034f7c
armando <abpprkonsalting@gmail.com>
2022-04-25 17:57:46 -0400

    some cleaning in NonReadyPersonalController

--- app/Http/Controllers/NonReadyPersonalController.php
+++ app/Http/Controllers/NonReadyPersonalController.php
@@ -4,18 +4,9 @@ namespace App\Http\Controllers;
 
 use App\DataTables\NonReadyPersonalDataTable;
 use App\Http\Controllers\AppBaseController;
-use Response;
-use App\Services\StatusService;
 
 class NonReadyPersonalController extends AppBaseController
 {
-
-    private $statusService;
-
-    public function __construct(StatusService $statusService)
-    {
-        $this->statusService = $statusService;
-    }
 
     public function index(NonReadyPersonalDataTable $nonReadyPersonalDataTable)
     {
