753a6e0cb2a057454a72d04dabbc02041d6111b6
armando <abpprkonsalting@gmail.com>
2022-04-25 22:29:01 -0400

    working in byCertification report

--- app/Http/Controllers/ByCertificationsController.php
+++ app/Http/Controllers/ByCertificationsController.php
@@ -2,35 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\DataTables\PersonalInformationDataTable;
 use App\DataTables\ByCertificationsDataTable;
-use App\Http\Requests;
-use App\Http\Requests\CreatePersonalInformationRequest;
-use App\Http\Requests\UpdatePersonalInformationRequest;
-use App\Repositories\PersonalInformationRepository;
-use Flash;
 use App\Http\Controllers\AppBaseController;
-use App\Models\PersonalInformation;
-use Response;
-use PDF;
-use Yajra\Datatables\Datatables;
 
 class ByCertificationsController extends AppBaseController
 {
-    /** @var  PersonalInformationRepository */
-    private $personalInformationRepository;
 
-    public function __construct(PersonalInformationRepository $personalInformationRepo)
-    {
-        $this->personalInformationRepository = $personalInformationRepo;
-    }
-
-    /**
-     * Display a listing of the PersonalInformation.
-     *
-     * @param ByCertificationsDataTable $personalInformationDataTable
-     * @return Response
-     */
     public function index(ByCertificationsDataTable $byCertificationsDataTable)
     {
         return $byCertificationsDataTable->render('by_certifications.index');
